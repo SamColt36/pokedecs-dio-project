19f0208c2d4d4daa3e00592768554c0fa9165e3d
SamColt36 <samuelss.contato@gmail.com>
2023-07-23 18:15:46 -0300

    first commit

---
+++ docs/README.txt
@@ -0,0 +1,3 @@
+O arquivo `script.sh` cria a árvore de diretórios e arquivos utilidados em projetos tailwindcss cujo auto utiliza. 
+
+O arquivo `compiler.sh` compila o arquivo de saída css gerado pelo tailwind.config.js
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import(tailwindcss).Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      screens: {
+        xl: { min: "1024px" },
+        // => @media and (min-width: 1024px) and (max-width: 1279px)
+
+        lg: { min: "768px", max: "1023px" },
+        // Tablets
+        // => @media and (min-width: 768px) and (max-width: 1023px)
+
+        // md: { min: "426px", max: "767px" },
+        // => @media and (min-width: 426px) and (max-width: 767px)
+
+        sm: { min: "300px", max: "767px" },
+        // Smartphones
+        // => @media and (min-width: 300px) and (max-width: 425px)
+      },
+      colors: {
+        verde: '#14A06F'
+      }
+    },
+  },
+  plugins: [],
+}
