035db1d371d8739eb6eff4c8df05613e37949505
SamColt36 <samuelss.contato@gmail.com>
2023-08-19 07:11:20 -0300

    Lista com novas requests por item de list

--- src/js/poke-api.js
+++ src/js/poke-api.js
@@ -1,10 +1,20 @@
 
 const pokeApi = {}
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+
+pokeApi.getPokemonDetail = (pokemon) => {
+	return fetch(pokemon.url).then((response) => response.json())
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
 	const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 	return fetch(url)
 		.then((response) => response.json())
 		.then((jsonBody) => jsonBody.results)
-		.catch((error) => console.log(error))
+		.then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+			.then((detailRequests) => Promise.all(detailRequests))
+			.then((pokemonsDetail) => {
+				console.log(pokemonsDetail)
+			})
+			.catch((error) => console.log(error))
 }
